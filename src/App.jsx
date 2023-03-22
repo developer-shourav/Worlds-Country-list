c5f5bd536f749eb0b00d681a1f653a64b05ce6e2
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-22 20:46:46 +0600

    counter removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+
 import './App.css'
 import { Countries } from './components/Countries/Countries'
 
@@ -9,34 +9,9 @@ function App() {
     <div className="App">
       
     <Countries> </Countries>
-    <Counter> </Counter>
     </div>
   )
 }
 
-function Counter() {
-  const [count, setCount] = useState(" ");
-  
-  const increaseCount = () => {
-    const value = Number(count);
-    const newValue = value + 1 ;
-    setCount(newValue)
-  }
-  const decreaseCount = () => {
-    const value = Number(count);
-    if( value > 0){
-      const newValue = value - 1 ;
-      setCount(newValue)
-    }
-  }
-  return(
-    <div>
-      <h1>I love You {count}</h1>
-      <button onClick={increaseCount}>Increase</button> 
-      <br/>
-      <br/>
-      <button onClick={decreaseCount}>Decrease</button>
-    </div>
-  )
-}
+
 export default App
