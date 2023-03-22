f5d5e1c4b4987eb5aab51cac894d587c8ce3a7f1
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-22 22:09:40 +0600

    rest countris added

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,26 @@ import { Countries } from './components/Countries/Countries'
 
 function App() {
 
-
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then( data => setCountries(data))
+  }, [])
+  console.log(countries);
   return (
     <div className="App">
-      
-    <Countries> </Countries>
+
+      <div className="users-container">
+        {
+          countries.map( country => <Countries country = {country} > </Countries>)
+        }
+
+      </div>
+  
     </div>
   )
 }
 
 export default App
+
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,13 @@
-export const  Countries = () => {
+export const  Countries = (props) => {
+    const {name, capital, flags, region} = props.country;
 
     return (
-        <div>
-            
-        </div>
+        <div className='user-div'>
+         <img style={{marginTop:"20px", borderRadius:"10px"}} src={flags.png} alt="" />
+        <h2>{name.common}</h2>
+        <h4>Capital: {capital}</h4>
+        <p><b>Region: {region}</b></p>
+   </div>  
     )
 }
 
