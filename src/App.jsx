347a09bb482cb0226cf4fe6aeb61a1c374b7f36c
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-22 21:32:43 +0600

    useless code reduction add final project init done

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,6 @@ function App() {
     <div className="App">
       
     <Countries> </Countries>
-    <ExternalUser> </ExternalUser>
-    </div>
-  )
-}
-
-
-const ExternalUser = () => {
-const [users, setUsers] = useState([]);
-useEffect( () => {
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(res => res.json())
-  .then(data => setUsers(data))
-},[])
-
-console.log(users);
-  return (
-    <div>
-      <h1>External user:</h1>
-      {
-        users.map( user => <li>Name: {user.name}</li>)
-      }
     </div>
   )
 }
