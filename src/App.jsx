dddb14dd332e12cda9d19b2aa963d704a40562c7
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-22 19:39:03 +0600

    increase and decrease function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Countries } from './components/Countries/Countries'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
@@ -15,15 +15,25 @@ function App() {
 }
 
 function Counter() {
-  const abc = useState(0);
-  console.log(abc);
+  const [count, setCount] = useState(" ");
+  
+  const increaseCount = () => {
+    const value = Number(count);
+    const newValue = value + 1 ;
+    setCount(newValue)
+  }
+  const decreaseCount = () => {
+    const value = Number(count);
+    const newValue = value - 1 ;
+    setCount(newValue)
+  }
   return(
     <div>
-      <h1>Counter:</h1>
-      <button>Increase</button> 
+      <h1>I love You {count}</h1>
+      <button onClick={increaseCount}>Increase</button> 
       <br/>
       <br/>
-      <button>Decrease</button>
+      <button onClick={decreaseCount}>Decrease</button>
     </div>
   )
 }
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -2,7 +2,7 @@ export const  Countries = () => {
 
     return (
         <div>
-            <h1>I Love You Lamisha Dadi.</h1>
+            
         </div>
     )
 }
