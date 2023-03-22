468fba3e4c6a84b55b93d2c20cda1dac355063c5
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-22 21:17:09 +0600

    main useEffect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Countries } from './components/Countries/Countries'
 
@@ -9,9 +10,29 @@ function App() {
     <div className="App">
       
     <Countries> </Countries>
+    <ExternalUser> </ExternalUser>
     </div>
   )
 }
 
+
+const ExternalUser = () => {
+const [users, setUsers] = useState([]);
+useEffect( () => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(data => setUsers(data))
+},[])
+
+console.log(users);
+  return (
+    <div>
+      <h1>External user:</h1>
+      {
+        users.map( user => <li>Name: {user.name}</li>)
+      }
+    </div>
+  )
+}
 
 export default App
